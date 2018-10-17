cb8fb55465bd0281bf11b0b5cba4a3b35dd3edb6
廖子豪 <1742034382@qq.com>
2018-10-17 10:53:24 +0800

    提交第一天的工作

--- src/main/java/com/example/demo/SpringBootGitApplication.java
+++ src/main/java/com/example/demo/SpringBootGitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGitApplication {
 
 	public static void main(String[] args) {
+		System.out.println("牛逼啊宝贝");
 		SpringApplication.run(SpringBootGitApplication.class, args);
 	}
 }
