3a2082a885174c33995a0629cc985a8726df5c01
廖子豪 <1742034382@qq.com>
2018-10-17 11:06:03 +0800

    臭傻逼

--- src/main/java/com/example/demo/SpringBootGitApplication.java
+++ src/main/java/com/example/demo/SpringBootGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGitApplication {
 
 	public static void main(String[] args) {
-		System.out.println("牛逼啊宝贝xixixiixiix");
+		System.out.println("牛逼啊宝贝xixisfsfsxiixiix");
 		SpringApplication.run(SpringBootGitApplication.class, args);
 	}
 }
